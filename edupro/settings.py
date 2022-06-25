4917db43194440de9df47b78b7805e87bce0f7c3
Ulugbek Erkinov <u.erkinov@inbox.ru>
2022-06-25 17:20:18 +0500

    done

--- edupro/settings.py
+++ edupro/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-74r$4d&6do(@y)f-4%glvw=uyqk0^p)ocu$w*@(-kbtr@a&k)3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://e-ducationpro.herokuapp.com/',
                  '127.0.0.1']
